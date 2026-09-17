a4dfc46ebd0ea79ba764df566e9ac43dcbe6a34f
Prajakta Patil <prajakta.patil@parallel-minds.com>
2026-09-17 15:48:48 +0530

    feat: update TagPractice application

---
+++ practice/tagpractice/app.js
@@ -0,0 +1,9 @@
+const appName = "TagPractice";
+const version = "1.0.0";
+
+function startApplication() {
+    console.log(`${appName} - Version ${version}`);
+    console.log("Application started successfully.");
+}
+
+startApplication();
